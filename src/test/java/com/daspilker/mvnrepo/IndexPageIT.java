7d4051b2eb050efc519f29c9487c226f35c6bb6a
Daniel Spilker <mail@daniel-spilker.com>
2012-10-03 23:55:24 +0200

    added WebDriver test

---
+++ src/test/java/com/daspilker/mvnrepo/IndexPageIT.java
@@ -0,0 +1,49 @@
+package com.daspilker.mvnrepo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertEquals;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.tagName;
+
+public class IndexPageIT {
+    private static final String BASE_URL = "http://localhost:8080/";
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        String display = System.getenv("DISPLAY");
+        System.out.println("DISPLAY=" + display);
+        if (display != null) {
+            firefoxBinary.setEnvironmentProperty("DISPLAY", display);
+        }
+        driver = new FirefoxDriver(firefoxBinary, null);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void test() {
+        driver.get(BASE_URL);
+
+        WebElement headline = driver.findElement(tagName("h1"));
+        assertEquals("Maven Repository Browser", headline.getText());
+
+        WebElement releasesLink = driver.findElement(linkText("Browse Release Repository"));
+        assertEquals(BASE_URL + "browser/releases/", releasesLink.getAttribute("href"));
+
+        WebElement snapshotsLink = driver.findElement(linkText("Browse Snapshot Repository"));
+        assertEquals(BASE_URL + "browser/snapshots/", snapshotsLink.getAttribute("href"));
+    }
+}
