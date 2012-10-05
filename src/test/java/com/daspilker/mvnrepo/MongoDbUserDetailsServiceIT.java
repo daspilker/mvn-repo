bb51ed9101b47695ca919f19b27ee53b1596cbbe
Daniel Spilker <mail@daniel-spilker.com>
2012-10-05 23:45:53 +0200

    use nosqlunit for testing

--- src/test/java/com/daspilker/mvnrepo/MongoDbUserDetailsServiceIT.java
+++ src/test/java/com/daspilker/mvnrepo/MongoDbUserDetailsServiceIT.java
@@ -1,15 +1,26 @@
+/*
+   Copyright 2012 Daniel A. Spilker
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package com.daspilker.mvnrepo;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import com.mongodb.MongoURI;
-import org.junit.Before;
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,43 +29,31 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
-import java.net.UnknownHostException;
 
-import static com.mongodb.BasicDBObjectBuilder.start;
-import static java.util.Collections.singletonList;
+import static com.lordofthejars.nosqlunit.core.LoadStrategyEnum.CLEAN_INSERT;
+import static com.lordofthejars.nosqlunit.mongodb.MongoDbConfigurationBuilder.mongoDb;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = MongoDbUserDetailsServiceIT.TestConfiguration.class)
+@ContextConfiguration
 public class MongoDbUserDetailsServiceIT {
+    @Rule
+    public MongoDbRule remoteMongoDbRule = new MongoDbRule(mongoDb().databaseName("mvnrepo-test").build());
+
     @Inject
     private UserDetailsService userDetailsService;
 
-    @Inject
-    private MongoDbFactory mongoDbFactory;
-
-    @Before
-    public void setUp() {
-        mongoDbFactory.getDb().getCollection("users").remove(new BasicDBObject("username", "daspilker"));
-    }
-
     @Test(expected = UsernameNotFoundException.class)
+    @UsingDataSet(loadStrategy = CLEAN_INSERT)
     public void testNotFound() {
         userDetailsService.loadUserByUsername("daspilker");
     }
 
     @Test
+    @UsingDataSet(loadStrategy = CLEAN_INSERT)
     public void test() {
-        DBObject user = start().
-                add("username", "daspilker").
-                add("password", "secret").
-                add("authorities", singletonList("ROLE_TEST")).
-                add("salt", "salted").
-                get();
-        mongoDbFactory.getDb().getCollection("users").insert(user);
-
         UserDetails result = userDetailsService.loadUserByUsername("daspilker");
 
         assertNotNull(result);
@@ -64,18 +63,5 @@ public class MongoDbUserDetailsServiceIT {
         assertTrue(result.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_TEST")));
         assertTrue(result instanceof MongoDbUserDetailsService.SaltedUser);
         assertEquals("salted", ((MongoDbUserDetailsService.SaltedUser) result).getSalt());
-    }
-
-    @Configuration
-    public static class TestConfiguration {
-        @Bean
-        public MongoDbFactory mongoDbFactory() throws UnknownHostException {
-            return new SimpleMongoDbFactory(new MongoURI("mongodb://localhost/mvnrepo-test"));
-        }
-
-        @Bean
-        public UserDetailsService userDetailsService() {
-            return new MongoDbUserDetailsService();
-        }
     }
 }
